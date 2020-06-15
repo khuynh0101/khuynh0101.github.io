5b518d72b531fe5b47d4e707c914ff09c521de2a
khuynh0101 <khuynh0101@gmail.com>
2020-06-15 16:55:10 -0400

    restyling

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
-function toggleMenu(){
-    const menu = document.querySelector('.nav-menu');
-    //open menu
-    if (menu.className === 'nav-menu' || menu.className === 'nav-menu nav-menu-close')
-    {
-        menu.className = 'nav-menu nav-menu-open';
+function init() {
+  try {
+    if (window.location.hash) {
+      const hash = window.location.hash;
+      const contentItems = document.querySelectorAll(
+        '.sidebar-content-item li a'
+      );
+      contentItems.forEach(function (contentItem) {
+        if (contentItem.hash === hash) contentItem.className = 'active';
+        else contentItem.className = '';
+      });
     }
-    else if ( menu.className === 'nav-menu nav-menu-open')
-    {
-        menu.className = 'nav-menu nav-menu-close';
-    }
+  } catch (exception) {}
+}
+
+function toggleMenu() {
+  const menu = document.querySelector('.sidebar');
+  //open menu
+  if (
+    menu.className === 'sidebar' ||
+    menu.className === 'sidebar sidebar-menu-close'
+  ) {
+    menu.className = 'sidebar sidebar-menu-open';
+  } else if (menu.className === 'sidebar sidebar-menu-open') {
+    menu.className = 'sidebar sidebar-menu-close';
+  }
+}
+function handleSideBarContentClick(content) {
+  const menu = document.querySelector('.sidebar');
+  menu.className = 'sidebar sidebar-menu-close';
+
+  const contentItems = document.querySelectorAll('.sidebar-content-item li a');
+  contentItems.forEach(function (contentItem) {
+    if (contentItem.hash === content.hash) contentItem.className = 'active';
+    else contentItem.className = '';
+  });
 }
