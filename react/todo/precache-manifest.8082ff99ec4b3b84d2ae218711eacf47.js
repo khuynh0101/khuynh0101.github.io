827801a1b4d13e02e0676329d72099ab98d91f4d
khuynh0101 <khuynh0101@gmail.com>
2020-06-16 13:26:34 -0400

    added react project-todo

---
+++ react/todo/precache-manifest.8082ff99ec4b3b84d2ae218711eacf47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1566783a635eb33416581abfb0c578d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed9fefafae6ed32614b4",
+    "url": "/static/css/main.791f8502.chunk.css"
+  },
+  {
+    "revision": "a0486010812e6b451cb2",
+    "url": "/static/js/2.e82c9323.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e82c9323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9fefafae6ed32614b4",
+    "url": "/static/js/main.3773743d.chunk.js"
+  },
+  {
+    "revision": "37a0d35451170c581fd7",
+    "url": "/static/js/runtime-main.c1487c63.js"
+  }
+]);
