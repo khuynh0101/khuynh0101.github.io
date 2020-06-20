f615e0db03b1aa13bbd849533629c5863a1910b5
khuynh0101 <khuynh0101@gmail.com>
2020-06-19 22:04:16 -0400

    added micro blog

---
+++ react/micro-blog/precache-manifest.d1859e13cd2a36bc02d35cf14991a5b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a038d5df2e4f87eb11be9ab55544446a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "344dfaba62f5c029bf64",
+    "url": "/static/css/main.f71a2f30.chunk.css"
+  },
+  {
+    "revision": "c50c619a782c868ac6dc",
+    "url": "/static/js/2.b20898cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b20898cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344dfaba62f5c029bf64",
+    "url": "/static/js/main.16d81e3d.chunk.js"
+  },
+  {
+    "revision": "c7504ebc7eb67c32766d",
+    "url": "/static/js/runtime-main.f8052b48.js"
+  }
+]);
