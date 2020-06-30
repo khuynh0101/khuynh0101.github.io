950c059f96d7ff7e70f1c391175e84125166bdce
khuynh0101 <khuynh0101@gmail.com>
2020-06-30 07:22:39 -0400

    publish

--- react/online-store/service-worker.js
+++ react/online-store/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ee4f409289ca0804ddab599f96cd23d.js"
+  "/precache-manifest.0bbc6a7d1ff77d6501e57e7b0801c8de.js"
 );
 
 self.addEventListener('message', (event) => {
